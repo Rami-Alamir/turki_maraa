d943e74ea79b79e57276147cbab800e6ddbc817d
Rami-Alamir <rami.alamir.7@gmail.com>
2023-01-15 14:54:35 +0300

    update cart

--- lib/presentation/screens/cart/shopping_cart.dart
+++ lib/presentation/screens/cart/shopping_cart.dart
@@ -8,16 +8,13 @@ import '../../widgets/cart/delivery_periods.dart';
 import '../../widgets/cart/note.dart';
 import '../../widgets/cart/payment_method.dart';
 import '../../widgets/cart/promo_code.dart';
-import '../../widgets/shared/not_auth.dart';
+import '../../widgets/shared/page_builder.dart';
 import '../../widgets/shared/primary_app_bar.dart';
 import '../../widgets/shared/empty_list.dart';
-import '../../widgets/shared/retry.dart';
-import '../../widgets/shared/spinkit_indicator.dart';
 import '../../../controllers/cart_provider.dart';
 import '../../../core/constants/fixed_assets.dart';
 import '../../../core/utilities/app_localizations.dart';
 import '../../../core/utilities/size_config.dart';
-import '../../../core/utilities/enum/request_status.dart';
 
 class ShoppingCart extends StatefulWidget {
   const ShoppingCart({Key? key}) : super(key: key);
@@ -42,51 +39,44 @@ class ShoppingCartState extends State<ShoppingCart> {
           title: AppLocalizations.of(context)!.tr('cart'),
           back: false,
         ),
-        body: !cart.isAuth
-            ? const NotAuth()
-            : cart.latLng == null
-                ? const EmptyList(
-                    image: FixedAssets.emptyCart,
-                    title: 'empty_cart',
-                  )
-                : cart.requestStatus == RequestStatus.isLoading
-                    ? const SpinkitIndicator()
-                    : cart.requestStatus == RequestStatus.error
-                        ? Retry(
-                            onPressed: () {
-                              cart.getCartData(isLoading: true);
-                            },
+        body: PageBuilder(
+          isAuth: cart.isAuth,
+          requestStatus: cart.requestStatus,
+          isEmpty: cart.latLng == null,
+          emptyStateImage: FixedAssets.emptyCart,
+          emptyStateTitle: 'empty_cart',
+          onError: () {
+            cart.getCartData(isLoading: true);
+          },
+          child: (cart.cartData?.data?.cart?.data?.length ?? 0) > 0
+              ? GestureDetector(
+                  onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+                  child: Stack(
+                    children: [
+                      ListView(
+                        controller: cart.scrollController,
+                        physics: const ScrollPhysics(),
+                        children: [
+                          const CartItemsList(),
+                          const DeliveryAddress(),
+                          const DeliveryDate(),
+                          const DeliveryPeriods(),
+                          const PaymentMethod(),
+                          const PromoCode(),
+                          const Note(),
+                          SizedBox(
+                            height: SizeConfig.screenHeight! * 0.25,
                           )
-                        : (cart.cartData?.data?.cart?.data?.length ?? 0) > 0
-                            ? GestureDetector(
-                                onTap: () => FocusScope.of(context)
-                                    .requestFocus(FocusNode()),
-                                child: Stack(
-                                  children: [
-                                    ListView(
-                                      controller: cart.scrollController,
-                                      physics: const ScrollPhysics(),
-                                      children: [
-                                        const CartItemsList(),
-                                        const DeliveryAddress(),
-                                        const DeliveryDate(),
-                                        const DeliveryPeriods(),
-                                        const PaymentMethod(),
-                                        const PromoCode(),
-                                        const Note(),
-                                        SizedBox(
-                                          height:
-                                              SizeConfig.screenHeight! * 0.25,
-                                        )
-                                      ],
-                                    ),
-                                    const CartBottomSheet()
-                                  ],
-                                ),
-                              )
-                            : const EmptyList(
-                                image: FixedAssets.emptyCart,
-                                title: 'empty_cart',
-                              ));
+                        ],
+                      ),
+                      const CartBottomSheet()
+                    ],
+                  ),
+                )
+              : const EmptyList(
+                  image: FixedAssets.emptyCart,
+                  title: 'empty_cart',
+                ),
+        ));
   }
 }
